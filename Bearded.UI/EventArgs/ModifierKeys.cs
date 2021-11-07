f872a314aa9714e4e5def30ed7c99e62037e6518
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-11-07 19:49:27 +0000

    ✨ Make ModifierKeys usable outside Bearded.UI library (#16)
    * :sparkles: Make it possible to instantiate empty ModifierKeys instance
    
    * :sparkles: Create ModifierKeys builder
    
    * :sparkles: Implement equality methods for ModifierKeys
    
    * :pencil: Change ModifierKeys to a readonly struct
    
    * :bug: Fix equality checks for ModifierKeys
    
    * :bug: Set language version to C# 8.0
    
    * :bug: Update Visual Studio version in appveyor config
    
    * 📝 Refactor ModifierKeys to treat L/R separately
    
    To avoid all the extra code repetition this would induce, this PR changes the underlying implementation to use a flags enum. Note that this is hidden as an implementation detail, so we can keep providing the rich layer of functionality on top.
    
    Another change that had to be made is that the `IsSupersetOf` is now ambiguous. In most cases, you just want to check if - say - a shift button is pressed, not caring about which side is really pressed. This is why I kept both this logic, and added a strict superset check too.
    
    * Revert "📝 Refactor ModifierKeys to treat L/R separately"
    
    This reverts commit d61d0b47b5f80957219a172c9a1c87bd69ae22ea.
    
    * ⛏ Replace builder with modification methods
    
    * Update Bearded.UI/EventArgs/ModifierKeys.cs
    
    Co-authored-by: Paul Scharf <amul@amulware.net>
    
    Co-authored-by: Paul Scharf <amul@amulware.net>

--- Bearded.UI/EventArgs/ModifierKeys.cs
+++ Bearded.UI/EventArgs/ModifierKeys.cs
@@ -1,10 +1,10 @@
+using System;
 using Bearded.Utilities.Input;
-using OpenTK.Windowing.Common.Input;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace Bearded.UI.EventArgs
 {
-    public sealed class ModifierKeys
+    public readonly struct ModifierKeys : IEquatable<ModifierKeys>
     {
         public bool Shift { get; }
         public bool Control { get; }
@@ -19,6 +19,8 @@ namespace Bearded.UI.EventArgs
             Win = win;
         }
 
+        public static ModifierKeys None => new ModifierKeys();
+
         public static ModifierKeys FromInputManager(InputManager inputManager)
         {
             return new ModifierKeys(
@@ -27,5 +29,30 @@ namespace Bearded.UI.EventArgs
                 inputManager.IsKeyPressed(Keys.LeftAlt) || inputManager.IsKeyPressed(Keys.RightAlt),
                 inputManager.IsKeyPressed(Keys.LeftSuper) || inputManager.IsKeyPressed(Keys.RightSuper));
         }
+
+        public ModifierKeys WithShift() => new ModifierKeys(true, Control, Alt, Win);
+
+        public ModifierKeys WithControl() => new ModifierKeys(Shift, true, Alt, Win);
+
+        public ModifierKeys WithAlt() => new ModifierKeys(Shift, Control, true, Win);
+
+        public ModifierKeys WithWin() => new ModifierKeys(Shift, Control, Alt, true);
+
+        public bool IsSupersetOf(ModifierKeys other) =>
+            (Shift && other.Shift) == other.Shift &&
+            (Control && other.Control) == other.Control &&
+            (Alt && other.Alt) == other.Alt &&
+            (Win && other.Win) == other.Win;
+
+        public bool Equals(ModifierKeys other) =>
+            Shift == other.Shift && Control == other.Control && Alt == other.Alt && Win == other.Win;
+
+        public override bool Equals(object? obj) => obj is ModifierKeys other && Equals(other);
+
+        public override int GetHashCode() => HashCode.Combine(Shift, Control, Alt, Win);
+
+        public static bool operator ==(ModifierKeys left, ModifierKeys right) => Equals(left, right);
+
+        public static bool operator !=(ModifierKeys left, ModifierKeys right) => !Equals(left, right);
     }
 }
